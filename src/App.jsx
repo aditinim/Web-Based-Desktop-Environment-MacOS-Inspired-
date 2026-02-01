d81cca4651491fc1f853ad1dcc291303bb0ae72d
Aditi Nim <aditinim@Aditis-MacBook-Air-2.local>
2026-02-01 18:07:18 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import "./app.scss";
 import Dock from './components/Dock';
 import Navbar from './components/Navbar';
-import MacWindow from './components/Windows/macWindow';
+// import MacWindow from './components/Windows/macWindow';
 import Github from './components/Windows/Github';
 import Note from './components/Windows/Note';
 import Resume from './components/Windows/Resume';
 import Spotify from './components/Windows/Spotify';
 import Cli from './components/Windows/Cli';
-import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+// import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
 
